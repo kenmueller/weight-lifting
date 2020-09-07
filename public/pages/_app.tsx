b16d1c20988615a20836f57063ebe9a1c6273344
Ken Mueller <kenmueller0@gmail.com>
2020-09-06 18:13:21 -0700

    toast

--- public/pages/_app.tsx
+++ public/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
 
 import 'styles/global.scss'
 
 const App = ({ Component, pageProps }: AppProps) => (
-	<Component {...pageProps} />
+	<>
+		<Head>
+			<link
+				key="font-preconnect"
+				rel="preconnect"
+				href="https://fonts.gstatic.com"
+			/>
+			<link
+				key="muli-font"
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css2?family=Muli:wght@400;700;900&display=swap"
+			/>
+		</Head>
+		<Component {...pageProps} />
+		<ToastContainer />
+	</>
 )
 
 export default App
