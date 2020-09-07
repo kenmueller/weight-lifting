c99b0336281a681148aee8b297696c2b26fed55c
Ken Mueller <kenmueller0@gmail.com>
2020-09-06 20:18:34 -0700

    getRoutine

---
+++ public/lib/getRoutine.ts
@@ -0,0 +1,25 @@
+import firebase from './firebase'
+import Routine from 'models/Routine'
+
+import 'firebase/firestore'
+
+const firestore = firebase.firestore()
+
+const getRoutine = async (id: string): Promise<Routine | null> => {
+	const snapshot = await firestore.doc(`routines/${id}`).get()
+	const owner = await firestore.doc(`users/${snapshot.get('owner')}`).get()
+	
+	return snapshot.exists
+		? {
+			id,
+			slug: snapshot.get('slug'),
+			name: snapshot.get('name'),
+			owner: {
+				id: owner.id,
+				name: owner.get('name')
+			}
+		}
+		: null
+}
+
+export default getRoutine
