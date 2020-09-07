6ac3d67a2f9793f628fddf1b5d5f2286d27d6648
Ken Mueller <kenmueller0@gmail.com>
2020-09-06 21:18:31 -0700

    useroutines loads routines

--- public/hooks/useRoutines.ts
+++ public/hooks/useRoutines.ts
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify'
 
 import firebase from 'lib/firebase'
 import Routine from 'models/Routine'
+import User from 'models/User'
 import useCurrentUser from './useCurrentUser'
 
 import 'firebase/firestore'
@@ -16,17 +17,53 @@ const useRoutines = () => {
 	useEffect(() => (
 		user
 			? firestore.collection(`users/${user.uid}/routines`).onSnapshot(
-				snapshot => setRoutines(_routines => {
-					const routines = [..._routines]
-					
-					// for (const { type, doc } of snapshot.docChanges())
-					// 	switch (type) {
-					// 		case 'added':
+				snapshot => {
+					for (const { type, doc } of snapshot.docChanges())
+						switch (type) {
+							case 'added': {
+								let added = false
 								
-					// 	}
-					
-					return routines
-				}),
+								firestore.doc(`routines/${doc.id}`).onSnapshot(
+									snapshot => setRoutines(routines => {
+										if (!snapshot.exists)
+											return routines.filter(({ id }) => id !== doc.id)
+										
+										if (added) {
+											const routine = routines.find(({ id }) => id === doc.id)
+											
+											if (!routine)
+												return routines
+											
+											routine.slug = snapshot.get('slug')
+											routine.name = snapshot.get('name')
+											routine.info = snapshot.get('info')
+											routine.description = snapshot.get('description')
+											
+											return [...routines]
+										}
+										
+										added = true
+										
+										return [...routines, {
+											id: snapshot.id,
+											slug: snapshot.get('slug'),
+											name: snapshot.get('name'),
+											info: snapshot.get('info'),
+											description: snapshot.get('description'),
+											owner: {} as User
+										}]
+									}),
+									error => toast.error(error.message)
+								)
+								break
+							}
+							case 'removed':
+								setRoutines(routines =>
+									routines.filter(({ id }) => id !== doc.id)
+								)
+								break
+						}
+				},
 				error => toast.error(error.message)
 			)
 			: undefined
